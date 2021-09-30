cd0085d0f39d87a7552cb41f7536795cf77b3fd7
Daryll M <djtm97@gmail.com>
2021-09-29 20:56:37 -0400

    update form and include event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-// $(document).ready(function(){
-//     $('select').formSelect();
-//   });
+var formEl = document.querySelector("#searchFlights");
+
+var getFlights = function (event) {
+    event.preventDefault();
+    var startCityInput = document.querySelector("#origin-city").value;
+    var endCityInput = document.querySelector("#destination-city").value;
+    var dateInput = document.querySelector("#dates").value;
+    var passNumInput = document.querySelector("#num-passengers").value;
+    console.log(startCityInput);
+    console.log(endCityInput);
+    console.log(dateInput);
+    console.log(passNumInput);
+}
+
+formEl.addEventListener("submit", getFlights);
