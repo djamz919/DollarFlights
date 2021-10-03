735105870ac935266e563b43f749a66ce93354d2
Daryll M <djtm97@gmail.com>
2021-10-03 19:15:59 -0400

    add display quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,14 @@ var formCodesEl = document.querySelector("#searchCodes");
 var formFlightsEl = document.querySelector("#searchFlights");
 
 var displayCodes = function (response) {
-  var codescontainerEl = document.getElementById("display-codes-container");
-  for (var i =0; i < response.Places.length; i++) {
+  var codesDivEl = document.getElementById("display-codes-container");
+  codesDivEl.innerHTML = '';
+  var codescontainerEl = document.createElement("div");
+  codescontainerEl.className = "col s12 light-blue accent-4";
+  var codesHeader = document.createElement('h3');
+  codesHeader.textContent = 'Airport Codes for ' + document.querySelector("#city-to-translate").value;
+  codescontainerEl.appendChild(codesHeader);
+  for (var i = 0; i < response.Places.length; i++) {
     console.log(response.Places[i]);
     var codeArr = response.Places[i].PlaceId.split("-");
     //console.log(codeArr);
@@ -11,6 +17,7 @@ var displayCodes = function (response) {
     airportCodes.textContent = codeArr[0] + ": " + response.Places[i].PlaceName;
     codescontainerEl.appendChild(airportCodes);
   }
+  codesDivEl.appendChild(codescontainerEl);
 }
 
 var getCodes = function (event) {
@@ -34,7 +41,64 @@ var getCodes = function (event) {
       console.log(response);
       displayCodes(response);
     });
+}
 
+var translatePlaceId = function (id, places) {
+  var airportName = "";
+  for (var i = 0; i < places.length; i++) {
+    // console.log(places[i].PlaceId);
+    // console.log(id);
+    if (id === places[i].PlaceId) {
+      airportName = places[i].Name;
+      // console.log(airportName);
+      return airportName;
+    }
+  }
+}
+
+var translateCarrierId = function (id, carrierIds) {
+  var airlineName = "";
+  for (var i = 0; i < carrierIds.length; i++) {
+    // console.log(places[i].PlaceId);
+    // console.log(id);
+    if (id === carrierIds[i].CarrierId) {
+      airlineName = carrierIds[i].Name;
+      // console.log(airlineName);
+      return airlineName;
+    }
+  }
+}
+
+var displayFlights = function (response) {
+  // Origin Airport to Destination Airport; Airline and Flight ID; Time of Departure; Cost
+  var displayFlightsContainerEl = document.getElementById("display-flights-container");
+  displayFlightsContainerEl.innerHTML = "";
+  console.log(response.Quotes.length);
+  for (var i = 0; i < response.Quotes.length; i++) {
+    var flightsCardEl = document.createElement('div');
+    flightsCardEl.className = "col s12 light-blue accent-4";
+    flightsCardEl.id = "flights" + i;
+    var flightHeaderEl = document.createElement('h3');
+    var flightAirlineEl = document.createElement('p');
+    var flightDateEl = document.createElement('p');
+    var flightCostEl = document.createElement('p');
+    // console.log(response.Quotes[i].OutboundLeg.OriginId);
+    // console.log(response.Places);
+    flightHeaderEl.textContent = translatePlaceId(response.Quotes[i].OutboundLeg.OriginId, response.Places) + " to " + translatePlaceId(response.Quotes[i].OutboundLeg.DestinationId, response.Places)
+    // console.log(flightHeaderEl.textContent);
+    flightHeaderEl.className = "col s8";
+    flightsCardEl.appendChild(flightHeaderEl);
+    flightDateEl.textContent = response.Quotes[i].OutboundLeg.DepartureDate;
+    flightDateEl.className = "col s4";
+    flightsCardEl.appendChild(flightDateEl);
+    flightAirlineEl.textContent = "Airline: " + translateCarrierId(response.Quotes[i].OutboundLeg.CarrierIds[0], response.Carriers);
+    flightAirlineEl.className = "col s12";
+    flightsCardEl.appendChild(flightAirlineEl);
+    flightCostEl.textContent = "Price: $" + response.Quotes[i].MinPrice;
+    flightCostEl.className = "col s12";
+    flightsCardEl.appendChild(flightCostEl);
+    displayFlightsContainerEl.appendChild(flightsCardEl);
+  }
 }
 
 var getFlights = function (event) {
@@ -42,13 +106,12 @@ var getFlights = function (event) {
   var startCityInput = document.querySelector("#origin-city").value;
   var endCityInput = document.querySelector("#destination-city").value;
   var dateInput = document.querySelector("#dates").value;
-  var passNumInput = document.querySelector("#num-passengers").value;
+  // var passNumInput = document.querySelector("#num-passengers").value;
   console.log(startCityInput);
   console.log(endCityInput);
   console.log(dateInput);
-  console.log(passNumInput);
-
-  fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/BKK-sky/JFK-sky/2021-10-15?inboundpartialdate=2021-10-30", {
+  // console.log(passNumInput);
+  fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + startCityInput + "-sky/" + endCityInput + "-sky/" + dateInput, {
     "method": "GET",
     "headers": {
       "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
@@ -63,6 +126,7 @@ var getFlights = function (event) {
     })
     .then(response => {
       console.log(response);
+      displayFlights(response);
     });
 
   // fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO/JFK/2021-10-10?inboundpartialdate=2021-10-25", {
